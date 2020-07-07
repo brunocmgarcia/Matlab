8b651f758910b6f8bc1af43862b42e9d41785927
brunocmgarcia <62027263+brunocmgarcia@users.noreply.github.com>
2020-07-07 22:19:31 +0200

    Update s200611_fooofed_Statistics.m

--- s200611_fooofed_Statistics.m
+++ s200611_fooofed_Statistics.m
@@ -9,7 +9,7 @@ TPs = {'TP00', 'TP01', 'TP02', 'TP03', 'TP04', 'TP06', 'TP08', 'TP11', 'TP14', '
 
 for cg_i = 1:length(CGs)
     CG = CGs{cg_i};
-    cd(['C:\Users\bruno\Documents\LR3\4bruno\4bruno\'  CG '_Ruhe' filesep 'fooofed'])     
+    cd(['C:\Users\bruno\Documents\LR3\4bruno\4bruno\'  CG '_Ruhe' filesep 'pwelch_results'])     
     for tp_i = 1:length (TPs)
         TP = TPs{tp_i};
         if isfile(['fooofed_Ruhe_M1_' TP '.mat'])
@@ -99,7 +99,7 @@ for tp_i = 1: length(TPs)
     TP = TPs{tp_i};
     stats_peak_center_avg.(TP).M1 = mean(cat(1, stats_peak_center.(TP).M1{:}));
     stats_peak_center_avg.(TP).STR = mean(cat(1, stats_peak_center.(TP).STR{:}));
-    stats_peak_center1_avg.(TP).SNR = mean(cat(1, stats_peak_center.(TP).SNR{:}));
+    stats_peak_center_avg.(TP).SNR = mean(cat(1, stats_peak_center.(TP).SNR{:}));
 
     stats_peak_power_avg.(TP).M1 = mean(cat(1, stats_peak_power.(TP).M1{:}));
     stats_peak_power_avg.(TP).STR = mean(cat(1, stats_peak_power.(TP).STR{:}));
@@ -107,8 +107,116 @@ for tp_i = 1: length(TPs)
 end
 
 %% Statistics comparing different time points
-%Wilcoxon signed rank test (signrank)
-p = signrank (cat(1, stats_peak_center.TP00.M1{:}), cat(1, stats_peak_center.TP31.M1{:}));
+% % One-sample Kolmogorov-Smirnov test 
+% %(for normality, if =1 then it rejects the null hypothesis that the data is normally distributed)
+%ktest for power
+kstest_power_m1  = kstest (cell2mat(stats_peak_power.TP00.M1(1, :))); 
+kstest_power_str = kstest (cell2mat(stats_peak_power.TP00.STR(1, :)));
+kstest_power_snr = kstest (cell2mat(stats_peak_power.TP00.SNR(1, :)));
 
+%ktest for center
+kstest_center_m1  = kstest (cell2mat(stats_peak_center.TP00.M1(1, :))); 
+kstest_center_str = kstest (cell2mat(stats_peak_center.TP00.STR(1, :)));
+kstest_center_snr = kstest (cell2mat(stats_peak_center.TP00.SNR(1, :)));
+
+for tp_i = 1: length(TPs)
+    TP = TPs{tp_i};
+    if ~strcmp (TP, 'TP06') 
+        if ~strcmp (TP, 'TP17')
+            %No Ruhe_CG02_TP17 and no Ruhe_CG04_TP17 data files, so these were not analysed.
+            %Since the signrank function requires equal number of elements to
+            %comparison the statistics for these 2 TP are not possible (at
+            %least for now, maybe there is a way - have to look for it)
+            
+%             %signrank for median peak power
+%             p_signrank.peak_power.M1.(TP) = signrank (cell2mat(stats_peak_power.TP00.M1(1, :)), cell2mat(stats_peak_power.(TP).M1(1, :)));
+%             p_signrank.peak_power.STR.(TP) = signrank (cell2mat(stats_peak_power.TP00.STR(1, :)), cell2mat(stats_peak_power.(TP).STR(1, :)));
+%             p_signrank.peak_power.SNR.(TP) = signrank (cell2mat(stats_peak_power.TP00.SNR(1, :)), cell2mat(stats_peak_power.(TP).SNR(1, :)));
+            
+            %signrank for median peak center
+            p_signrank.peak_center.M1.(TP) = ttest (cell2mat(stats_peak_center.TP00.M1(1, :)), cell2mat(stats_peak_center.(TP).M1(1, :)));
+            p_signrank.peak_center.STR.(TP) = ttest (cell2mat(stats_peak_center.TP00.STR(1, :)), cell2mat(stats_peak_center.(TP).STR(1, :)));
+            p_signrank.peak_center.SNR.(TP) = ttest (cell2mat(stats_peak_center.TP00.SNR(1, :)), cell2mat(stats_peak_center.(TP).SNR(1, :)));
+        end
+    end
+end
 
 %% Plot
+
+%Plot peak center avg M1 
+for tp_i = 1:length(TPs)
+    TP = TPs{tp_i};
+    center_m1(tp_i) = stats_peak_center_avg.(TP).M1;
+end
+figure ('Name', 'Peak Center Frequency - M1')
+bar (center_m1)
+xticklabels(TPs);
+ylabel('Frequency (Hz)')
+
+%Plot peak center avg STR 
+for tp_i = 1:length(TPs)
+    TP = TPs{tp_i};
+    center_str(tp_i) = stats_peak_center_avg.(TP).STR;
+end
+figure ('Name', 'Peak Center Frequency - STR')
+bar (center_str)
+xticklabels(TPs);
+ylabel('Frequency (Hz)')
+
+%Plot peak center avg SNR 
+for tp_i = 1:length(TPs)
+    TP = TPs{tp_i};
+    center_snr(tp_i) = stats_peak_center_avg.(TP).SNR;
+end
+figure ('Name', 'Peak Center Frequency - SNR')
+bar (center_snr)
+xticklabels(TPs);
+ylabel('Frequency (Hz)')
+
+
+
+%Plot peak power avg M1 
+for tp_i = 1:length(TPs)
+    TP = TPs{tp_i};
+    power_m1(tp_i) = stats_peak_power_avg.(TP).M1;
+end
+figure ('Name', 'Peak Power - M1')
+bar (power_m1)
+xticklabels(TPs);
+
+%Plot peak power avg STR 
+for tp_i = 1:length(TPs)
+    TP = TPs{tp_i};
+    power_str(tp_i) = stats_peak_power_avg.(TP).STR;
+end
+figure ('Name', 'Peak Power - STR')
+bar (power_str)
+xticklabels(TPs);
+
+%Plot peak power avg SNR 
+for tp_i = 1:length(TPs)
+    TP = TPs{tp_i};
+    power_snr(tp_i) = stats_peak_power_avg.(TP).SNR;
+end
+figure ('Name', 'Peak Power - SNR')
+bar (power_snr)
+xticklabels(TPs);
+
+%Plot overlap fooofed power spectra
+figure1= figure ('Name', 'M1 Power - CG04');
+% for cg_i = 1:length(CGs)
+% newcolors = {'#F00','#F01','#F20','#F30','#F40','#F50','#F60','#F70','#F80','#F90','#F10','#F11','#F12','#F13'};
+% colororder(newcolors)
+%         CG = CGs{cg_i};
+for tp_i = 1: length(TPs)
+    TP = TPs{tp_i};
+    if ~strcmp (TP, 'TP06')
+        plot (all_fooof_stats.CG04.(TP).M1.freqs, all_fooof_stats.CG04.(TP).M1.fooofed, 'Color', [0.9 0.1 0.1] );
+        hold on
+    end
+end
+hold off
+legend(TPs)
+
+% end
+
